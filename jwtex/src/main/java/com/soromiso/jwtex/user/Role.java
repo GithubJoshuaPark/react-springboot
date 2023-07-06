0b658585620ef91139ce13d7381994fa33fd3423
JoshuaPark <soromiso@gmail.com>
2023-07-06 19:01:52 +0900

    jwtex

---
+++ jwtex/src/main/java/com/soromiso/jwtex/user/Role.java
@@ -0,0 +1,6 @@
+package com.soromiso.jwtex.user;
+
+public enum Role {
+  USER,
+  ADMIN
+}
--- jwtex/src/main/java/com/soromiso/jwtex/user/User.java
+++ jwtex/src/main/java/com/soromiso/jwtex/user/User.java
@@ -1,14 +1,18 @@
 package com.soromiso.jwtex.user;
 
 import java.util.Collection;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
@@ -31,35 +35,32 @@ public class User implements UserDetails{
   private String email;
   private String password;
 
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+    return List.of(new SimpleGrantedAuthority(role.name()));
   }
   @Override
   public String getUsername() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+    return email;
   }
   @Override
   public boolean isAccountNonExpired() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'isAccountNonExpired'");
+    return true;
   }
   @Override
   public boolean isAccountNonLocked() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'isAccountNonLocked'");
+    return true;
   }
   @Override
   public boolean isCredentialsNonExpired() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'isCredentialsNonExpired'");
+    return true;
   }
   @Override
   public boolean isEnabled() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
+    return true;
   }
 
 }
