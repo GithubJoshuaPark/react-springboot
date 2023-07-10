fb5a53960e56538cddc01e09974d304588e409bd
JoshuaPark <soromiso@gmail.com>
2023-07-10 09:49:43 +0900

    modified jwtex

--- jwtex/readme.md
+++ jwtex/readme.md
@@ -6,3 +6,4 @@
 > [ref Spring Boot 3 + Spring Security 6 - JWT Authentication and Authorisation [NEW] [2023]](https://www.youtube.com/watch?v=KxqlJblhzfI&t=22s) > [ref Spring Boot 에서 Spring Security 기반 JWT 적용하기](https://velog.io/@soyeon207/JWT-%EC%8B%A4%EC%8A%B5) > [ref blog source](https://github.com/soyeon207/blog_example/blob/master/jwt-security-server/src/main/java/velog/soyeon/jwt/entity/Users.java)
 
 ---
+![ref spring boot secutiry, jwt](jwt.png)
--- jwtex/src/main/java/com/soromiso/jwtex/config/JwtAuthenticationFilter.java
+++ jwtex/src/main/java/com/soromiso/jwtex/config/JwtAuthenticationFilter.java
@@ -7,20 +7,40 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Configurable
+import com.soromiso.jwtex.service.JwtService;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+    private final JwtService jwtService;
 
     @Override
     protected void doFilterInternal(
-        HttpServletRequest request, 
-        HttpServletResponse response, 
-        FilterChain filterChain)
-            throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail; // email of the user who sent the request
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response); // let the next filter handle the request
+            return;
+        }
+
+        // remove "Bearer " from the header
+        jwt = authHeader.substring(7);
+
+        // extract the email(User entity has email property and getUsername() which return email property) from the jwt
+        userEmail = jwtService.extractUserName(jwt);
+
     }
-    
+
 }
